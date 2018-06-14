1f42c9eabf25c8fafbf7e4c5a0c50fc42c98e715
Luke Ledvina <soulgp1@gmail.com>
2018-06-14 14:03:25 -0400

    did some more stuff

--- app/src/main/java/com/example/lukeledvina/uielementsexamples/MainActivity.java
+++ app/src/main/java/com/example/lukeledvina/uielementsexamples/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.lukeledvina.uielementsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        System.out.println("This text is created in the onCreate");
+
+        setListener();
+
+    }
+
+    private void setListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //take input from edit ext and put it in <blank> view
+                textView.setText(editText.getText());
+            }
+        });
     }
 }
